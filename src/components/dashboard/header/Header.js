95c217a9b0ad6a5db3f056a432f4d52d00be7938
Serhii Klymets <ksd2000@ukr.net>
2023-09-07 02:47:37 +0300

    подключил модалки edit  +  exit

--- src/components/dashboard/header/Header.js
+++ src/components/dashboard/header/Header.js
@@ -1,9 +1,26 @@
+import { getModalState } from 'redux/transactions/selectors';
+import { toggleLogOutModal } from 'redux/modal/ModalSlice';
+import { getModalTypeState } from 'redux/modal/selectors';
+import Modal from '../../Modal/Modal';
+import Logout from '../../Logout/Logout';
 import { HeaderStyled } from './Header.styled';
 
 export const Header = () => {
+  const { useDispatch, useSelector } = require('react-redux');
+  const dispatch = useDispatch();
+  const modalType = useSelector(getModalTypeState);
+  const isModalOpen = useSelector(getModalState);
+
     return (
     <HeaderStyled>
       <h2>Логотип, имя, Exit</h2>
+      <button type="button" onClick={() => dispatch(toggleLogOutModal())}>
+        Exit
+      </button>
+
+      {modalType === 'modal/toggleLogOutModal' && isModalOpen && (
+          <Modal children={Logout()} />
+      )}
     </HeaderStyled>
   );
 }
