ec8d0df003fb5b3b382978fdd156cea91d29cf6e
Casimova Marina <maracasimova@gmail.com>
2023-09-09 22:27:20 +0300

    ButtonClose for Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -6,13 +6,12 @@ import { toggleModal } from 'redux/modal/ModalSlice';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default function Modal({ children }) {
+export default function Modal({ children, showCloseIcon = true }) {
   const dispatch = useDispatch();
 
   useEffect(() => {
     const handleKeyDown = e => {
       if (e.code === 'Escape') {
-        console.log({ children });
         dispatch(toggleModal());
       }
     };
@@ -21,7 +20,7 @@ export default function Modal({ children }) {
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
-  }, [dispatch, children]);
+  }, [dispatch]);
 
   const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
@@ -38,21 +37,20 @@ export default function Modal({ children }) {
   return createPortal(
     <Overlay onClick={handleBackdropClick}>
       <ModalWindow>
-        <ButtonClose type="button" name="closeSvg" onClick={closeClick}>
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 18 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M1 1L17 17" stroke="#FBFBFB" />
-            <path d="M1 17L17 0.999999" stroke="#FBFBFB" />
-          </svg>
-          {/* <svg width="16px" height="16px">
-            <use href="../icons/close.svg" />
-          </svg> */}
-        </ButtonClose>
+        {showCloseIcon && (
+          <ButtonClose type="button" name="closeSvg" onClick={closeClick}>
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 18 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M1 1L17 17" stroke="#FBFBFB" />
+              <path d="M1 17L17 0.999999" stroke="#FBFBFB" />
+            </svg>
+          </ButtonClose>
+        )}
         {children}
         <CancelBtn type="button" name="cancel" onClick={closeClick}>
           Cancel
