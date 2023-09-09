6dee0f9a98deb13240f68a40901b0207a79bbf9e
Casimova Marina <maracasimova@gmail.com>
2023-09-09 09:55:36 +0300

    Modal settings

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
-import { ModalWindow, Overlay, ButtonClose, CancelBtn } from './Modal.styled';
+import { Overlay, ModalWindow, ButtonClose, CancelBtn } from './Modal.styled';
 import { useDispatch } from 'react-redux';
 import { toggleModal } from 'redux/modal/ModalSlice';
 
