9f43940a233c4a5d0511066fff8547d0470b6de5
Andrew <93095765+ANPA21@users.noreply.github.com>
2023-09-09 23:31:18 +0300

    Update Add.js
    removed unused code

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -56,18 +56,6 @@ const initialValues = {
   comment: '',
 };
 
-const categories = [
-  { value: 'Main expenses', label: 'Main expenses' },
-  { value: 'Products', label: 'Products' },
-  { value: 'Car', label: 'Car' },
-  { value: 'Self care', label: 'Self care' },
-  { value: 'Child care', label: 'Child care' },
-  { value: 'Household products', label: 'Household products' },
-  { value: 'Education', label: 'Education' },
-  { value: 'Leisure', label: 'Leisure' },
-  { value: 'Other expenses', label: 'Other expenses' },
-  { value: 'Entertainment', label: 'Entertainment' },
-];
 
 const CustomInput = forwardRef(({ value, onClick }, ref) => (
   <>
