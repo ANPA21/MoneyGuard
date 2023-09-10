aff57fb6ceda394753d604f54d377f229d2673eb
Andrew <papsujevicandrew@gmail.com>
2023-09-10 21:53:27 +0300

    fix

--- src/components/dashboard/dynamic_sidebar/currency/Currency.js
+++ src/components/dashboard/dynamic_sidebar/currency/Currency.js
@@ -3,10 +3,13 @@ import { Table, Row, Cell } from './Currency.styled';
 import { selectCurrency } from 'redux/currencyReducer/currencySelectors';
 import { fetchCurrency } from 'redux/currencyReducer/currencyOperations';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { useAuth } from 'hooks';
+import { selectIsLoading } from 'redux/authReducer/selectors';
+import { SpinnerLoader } from 'components/Spinner/Spinner';
+import chart from '../../../../images/chart/chart-tablet.png';
 export const Currency = () => {
   const dispatch = useDispatch();
-
+  const isLoading = useSelector(selectIsLoading);
   useEffect(() => {
     const dataCurrencyStore = localStorage.getItem('persist:currency');
     if (dataCurrencyStore) {
@@ -22,28 +25,36 @@ export const Currency = () => {
   }, [dispatch]);
 
   const currency = useSelector(selectCurrency);
-
   return (
     <div>
-      <Table>
-        <Row>
-          <Cell>Currency</Cell>
-          <Cell>Purchase</Cell>
-          <Cell>Sale</Cell>
-        </Row>
-        <Row>
-          <Cell>USD</Cell>
-          <Cell>{currency.USD?.buy.toFixed(2)}</Cell>
-          <Cell>{currency.USD?.sale.toFixed(2)}</Cell>
-        </Row>
-        <Row>
-          <Cell>EUR</Cell>
-          <Cell>{currency.EUR?.buy.toFixed(2)}</Cell>
-          <Cell>{currency.EUR?.sale.toFixed(2)}</Cell>
-        </Row>
-        {/* Add more rows if needed */}
-      </Table>
-      <img src="../../../../images/chart/chart-tablet@2x.png" alt="Currency" />
+      {!isLoading ? (
+        <div>
+          <Table>
+            <Row>
+              <Cell>Currency</Cell>
+              <Cell>Purchase</Cell>
+              <Cell>Sale</Cell>
+            </Row>
+            <Row>
+              <Cell>USD</Cell>
+              <Cell>{currency.USD.buy}</Cell>
+              <Cell>{currency.USD.sale}</Cell>
+              <Cell></Cell>
+              <Cell></Cell>
+            </Row>
+            <Row>
+              <Cell>EUR</Cell>
+              <Cell>{currency.EUR.buy}</Cell>
+              <Cell>{currency.EUR.sale}</Cell>
+              <Cell></Cell>
+              <Cell></Cell>
+            </Row>
+          </Table>
+          <img src={chart} alt="Currency" />
+        </div>
+      ) : (
+        <SpinnerLoader />
+      )}
     </div>
   );
 };
