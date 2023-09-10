6918abf08d5b76ed4c1701962b1f8e017e4daca6
Andrew <papsujevicandrew@gmail.com>
2023-09-10 18:23:21 +0300

    typo fix navigation link

--- src/components/dashboard/dynamic_sidebar/navigation/Navigation.js
+++ src/components/dashboard/dynamic_sidebar/navigation/Navigation.js
@@ -31,7 +31,7 @@ export const Navigation = () => {
               </ActiveNavLink>
             </li>
             <li>
-              <ActiveNavLink to="/statistic">
+              <ActiveNavLink to="/statistics">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 32 32"
