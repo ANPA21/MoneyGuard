ee844e6d84099f9e85bc5dead19fc895ce80796b
Assel Shingissova <asselshin@gmail.com>
2023-09-10 19:36:37 +0600

    editimprovements
    removed from redux-categorySlice, updated Edit form, and added props in Homepage

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -19,24 +19,30 @@ import { toggleModal } from 'redux/modal/ModalSlice';
 import { CustomSelect } from 'components/Add/SelectCategory/SelectCategory.js';
 import { CustomSwitch } from 'components/CustomElements/CustomSwitch/CustomSwitch.js';
 import { RiCalendar2Fill } from 'react-icons/ri';
-// import { getCategoryState } from 'redux/transactions/selectors';
-//!В комменты что бы не ругалась проверка
-import { useDispatch } from 'react-redux';
-// import { addTransaction } from 'redux/transactions/operations';
+import { useDispatch, useSelector } from 'react-redux';
+import { editItem } from 'redux/transactionsRedux/transactionsOperations.js';
+import { selectorTransactions } from 'redux/transactionsRedux/transactionsSelectors.js';
 
 const addSchema = object({
   value: number().positive().required('Amount is required'),
-  comment: string().max(30, 'Maximum must be 30 characters'),
-  category: string().min(3).oneOf(['fuel', 'leusure']),
+  comment: string()
+    .max(30, 'Maximum must be 30 characters')
+    .required('Please fill in comment'),
+  category: string()
+    .min(3)
+    .oneOf([
+      'Main expenses',
+      'Products',
+      'Car',
+      'Self care',
+      'Child care',
+      'Household products',
+      'Education',
+      'Leisure',
+      'Other expenses',
+      'Entertainment',
+    ]),
 });
-
-const initialValues = {
-  type: '',
-  category: '',
-  value: '',
-  date: new Date(),
-  comment: '',
-};
 
 const CustomInput = forwardRef(({ value, onClick }, ref) => (
   <>
@@ -47,12 +53,22 @@ const CustomInput = forwardRef(({ value, onClick }, ref) => (
   </>
 ));
 
-export default function EditTransaction() {
+export default function EditTransaction({ id }) {
   const dispatch = useDispatch();
-
+  const transactions = useSelector(selectorTransactions);
   const [categories, setCategories] = useState(() => {
     return JSON.parse(window.localStorage.getItem('categories')) ?? [];
   });
+
+  const transaction = transactions.find(transaction => transaction._id === id);
+
+  const initialValues = {
+    type: transaction.type,
+    category: transaction.category ? transaction.category : '',
+    value: transaction.value,
+    date: new Date(transaction.date),
+    comment: transaction.comment,
+  };
 
   const getCategories = async () => {
     try {
@@ -67,9 +83,8 @@ export default function EditTransaction() {
     if (categories.length === 0) {
       getCategories();
     }
-
     localStorage.setItem('categories', JSON.stringify(categories));
-  }, [categories]);
+  }, [categories, transactions]);
 
   const optionCategories = categories.map(category => {
     return {
@@ -78,14 +93,10 @@ export default function EditTransaction() {
     };
   });
 
-  const handleSubmit = (values, { resetForm }) => {
-    const { type, category, value, date, comment } = values;
+  const handleSubmit = values => {
     console.log(values);
-    // Второй консоль лог для того что бы проверка не ругалась, когда все настроим, удалим
-    console.log(type, category, value, date, comment);
-    // dispatch(addTransaction(type, category, value, date, comment));
+    dispatch(editItem(id, values));
     dispatch(toggleModal());
-    // resetForm();
   };
 
   return (
@@ -122,7 +133,7 @@ export default function EditTransaction() {
             )}
             <Wrapper>
               <Label>
-                <StyledSum type="number" name="value" placeholder="0.00" />
+                <StyledSum type="number" name="value" />
                 <ErrorMessage name="value" component="div" />
               </Label>
               <Label>
@@ -143,11 +154,7 @@ export default function EditTransaction() {
             </Wrapper>
 
             <StyledLabel>
-              <StyledComment
-                type="textarea"
-                name="comment"
-                placeholder="Comment"
-              />
+              <StyledComment type="textarea" name="comment" />
               <ErrorMessage name="comment" component="div" />
             </StyledLabel>
 
