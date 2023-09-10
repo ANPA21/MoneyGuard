7d52d07cdc3a8e9f807e3b1a3b45e75b7ec5f6b6
Andrew <papsujevicandrew@gmail.com>
2023-09-10 13:31:14 +0300

    statistics table initial design

--- src/components/Statistics/StatisticsDataTable.js
+++ src/components/Statistics/StatisticsDataTable.js
@@ -1,5 +1,12 @@
 import {
+  Body,
   ColorBox,
+  Container,
+  Expenses,
+  Footer,
+  Head,
+  HeadText,
+  Income,
   RowText,
   StyledTable,
   StyledTdCat,
@@ -13,14 +20,53 @@ import { colors } from './statiscticsColors';
 
 export const DataTable = ({ reduxData }) => {
   return (
-    <StyledTable>
-      <thead>
-        <tr>
-          <StyledThCat>Category</StyledThCat>
-          <StyledThSum>Sum</StyledThSum>
-        </tr>
-      </thead>
-      <tbody>
+    // <StyledTable>
+    //   <thead>
+    //     <tr>
+    //       <StyledThCat>Category</StyledThCat>
+    //       <StyledThSum>Sum</StyledThSum>
+    //     </tr>
+    //   </thead>
+    //   <tbody>
+    //     {reduxData.categoryExpenses
+    //       .filter(category => category.total !== '0.00')
+    //       .map(category => {
+    //         const boxColor = colors.find(color => color.name === category.name);
+    //         return (
+    //           <StyledTr key={category.name}>
+    //             <StyledTdCat>
+    //               <ColorBox color={boxColor.color} />
+    //               <RowText>
+    //                 <span>{category.name}</span>
+    //                 <StyledTdSum>{category.total}</StyledTdSum>
+    //               </RowText>
+    //             </StyledTdCat>
+    //           </StyledTr>
+    //         );
+    //       })}
+    //   </tbody>
+    //   <tfoot>
+    //     <tr>
+    //       <td>Expenses</td>
+    //       <StyledTdTotal>{reduxData.totalExpenses}</StyledTdTotal>
+    //     </tr>
+    //     <tr>
+    //       <td>Income</td>
+    //       <StyledTdTotal>{reduxData.totalIncome}</StyledTdTotal>
+    //     </tr>
+    //   </tfoot>
+    // </StyledTable>
+
+    <Container>
+      <Head>
+        <HeadText>
+          <span>Category</span>
+        </HeadText>
+        <HeadText>
+          <span>Sum</span>
+        </HeadText>
+      </Head>
+      <Body>
         {reduxData.categoryExpenses
           .filter(category => category.total !== '0.00')
           .map(category => {
@@ -37,17 +83,17 @@ export const DataTable = ({ reduxData }) => {
               </StyledTr>
             );
           })}
-      </tbody>
-      <tfoot>
-        <tr>
-          <td>Expenses</td>
+      </Body>
+      <Footer>
+        <Expenses>
+          <span>Expenses:</span>
           <StyledTdTotal>{reduxData.totalExpenses}</StyledTdTotal>
-        </tr>
-        <tr>
-          <td>Income</td>
+        </Expenses>
+        <Income>
+          <span>Income:</span>
           <StyledTdTotal>{reduxData.totalIncome}</StyledTdTotal>
-        </tr>
-      </tfoot>
-    </StyledTable>
+        </Income>
+      </Footer>
+    </Container>
   );
 };
--- src/components/Statistics/statisticsDataTable.styled.js
+++ src/components/Statistics/statisticsDataTable.styled.js
@@ -1,33 +1,60 @@
 import styled from 'styled-components';
-
-export const StyledTable = styled.table`
+export const Container = styled.div`
+  padding: 0px 16px 0px 16px;
   width: 395px;
-  border-spacing: 16px 28px;
+`;
+export const Head = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  border-radius: 8px;
+  background: rgba(82, 59, 126, 0.6);
+  box-shadow: 0px 4px 60px 0px rgba(0, 0, 0, 0.25);
+  backdrop-filter: blur(50px);
+  padding: 16px 28px 16px 28px;
+`;
+export const HeadText = styled.div``;
+
+export const Body = styled.div``;
+export const Footer = styled.div``;
+export const Expenses = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px 28px 8px 28px;
+`;
+export const Income = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 28px 16px 28px;
 `;
 
 export const ColorBox = styled.div`
-  width: 24px;
-  height: 24px;
+  display: block;
+  min-width: 24px;
+  min-height: 24px;
   background-color: ${props => props.color};
   border-radius: 2px;
-
-  margin-right: 16px;
 `;
 
-export const StyledTdCat = styled.td`
+export const StyledTdCat = styled.div`
   display: flex;
   align-items: center;
 `;
 
-export const StyledTdSum = styled.td`
+export const StyledTdSum = styled.div`
   text-align: end;
 `;
 
-export const StyledTr = styled.tr`
-  border-bottom: 1px solid rgba(255, 255, 255, 0.5);
-  background-color: teal;
+export const StyledTr = styled.div`
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+  padding: 16px 28px 16px 28px;
 `;
-export const StyledThCat = styled.th`
+export const StyledThCat = styled.div`
   text-align: start;
 `;
 export const RowText = styled.div`
@@ -35,11 +62,12 @@ export const RowText = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  margin-left: 16px;
 `;
-export const StyledThSum = styled.th`
+export const StyledThSum = styled.div`
   text-align: end;
 `;
 
-export const StyledTdTotal = styled.td`
+export const StyledTdTotal = styled.div`
   text-align: end;
 `;
