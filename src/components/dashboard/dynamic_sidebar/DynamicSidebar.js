e92e20b0d4eabf34145c651642d0084760de8e06
Andrew <papsujevicandrew@gmail.com>
2023-09-11 23:32:37 +0300

    Update DynamicSidebar.js

--- src/components/dashboard/dynamic_sidebar/DynamicSidebar.js
+++ src/components/dashboard/dynamic_sidebar/DynamicSidebar.js
@@ -1,7 +1,7 @@
 import { DynamicSidebarStyled } from './DynamicSidebar.styled';
 import { Navigation } from './navigation/Navigation';
-import { Balance } from './balance/Balance';
-import { Currency } from './currency/Currency';
+// import { Balance } from './balance/Balance';
+// import { Currency } from './currency/Currency';
 
 export const DynamicSidebar = () => {
   return (
