ccef5ea9eda7fe496fca8dedc903ac1c9b17a6ec
Casimova Marina <maracasimova@gmail.com>
2023-09-12 10:50:03 +0300

    Dashbord/balance styles

--- src/components/dashboard/dynamic_sidebar/balance/Balance.js
+++ src/components/dashboard/dynamic_sidebar/balance/Balance.js
@@ -1,32 +1,34 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setBalance } from 'redux/balance/balance.Slice';
 import { selectorTransactions } from 'redux/transactionsRedux/transactionsSelectors';
 import { selectorBalance } from 'redux/balance/balance.Selectors';
-import { BalanceStyled } from './Balance.styled';
+import { BalanceStyled, Text, Line, Symbol, Total } from './Balance.styled';
 
 export const Balance = () => {
-    const dispatch = useDispatch();
-    const transactions = useSelector(selectorTransactions);
-    const balance = useSelector(selectorBalance);
+  const dispatch = useDispatch();
+  const transactions = useSelector(selectorTransactions);
+  const balance = useSelector(selectorBalance);
 
-    useEffect(() => {
-        const totalSum = transactions.reduce((sumValue, transaction) => {
-            let numberValue = parseFloat(transaction.value);
-            let numberSign = 1;
-                if (transaction.type === "expense") {
-                    numberSign = -1;
-                }
-            return sumValue + numberSign*numberValue;
-        }, 0);
-        dispatch(setBalance(totalSum))
-    }, [dispatch,transactions]);
+  useEffect(() => {
+    const totalSum = transactions.reduce((sumValue, transaction) => {
+      let numberValue = parseFloat(transaction.value);
+      let numberSign = 1;
+      if (transaction.type === 'expense') {
+        numberSign = -1;
+      }
+      return sumValue + numberSign * numberValue;
+    }, 0);
+    dispatch(setBalance(totalSum));
+  }, [dispatch, transactions]);
 
-    return (
-      <BalanceStyled>
-        <h3>Your balance</h3>
-        <p className="symbol">₴ </p>
-        <p className="text">{balance}</p>
-      </BalanceStyled>
-    );
-}
+  return (
+    <BalanceStyled>
+      <Text>Your balance</Text>
+      <Line>
+        <Symbol>₴</Symbol>
+        <Total>{balance}</Total>
+      </Line>
+    </BalanceStyled>
+  );
+};
