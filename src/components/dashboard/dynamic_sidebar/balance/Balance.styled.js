aaccc05474c3d0c38add2345cd608b31716852a7
Omelianov Nicolai <omelianov.n@gmail.com>
2023-09-13 04:10:43 +0300

    added media queries and fixed bags

--- src/components/dashboard/dynamic_sidebar/balance/Balance.styled.js
+++ src/components/dashboard/dynamic_sidebar/balance/Balance.styled.js
@@ -1,20 +1,32 @@
 import styled from 'styled-components';
 
 export const BalanceStyled = styled.div`
-  width: 280px;
+  
   height: 80px;
   background: rgba(82, 59, 126, 0.6);
   box-shadow: 0px 4px 60px 0px rgba(0, 0, 0, 0.25);
   backdrop-filter: blur(50px);
   border-radius: 8px;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-top: 15px;
+  margin-bottom: 32px;
+
+  @media screen and (max-width: 768px) {
+  max-width: 280px;
+  width: 100%;
+  }
+
 
   @media screen and (min-width: 768px) and (max-width: 1279px) {
     width: 336px;
+    margin: 28px 32px 0 35px;
   }
 
   @media screen and (min-width: 1280px) {
     border-radius: 0;
     width: 480px;
+    margin: 28px 0 32px 0;
     backdrop-filter: none;
     box-shadow: 1px 9px 15px 0px rgba(0, 0, 0, 0.2);
   }
@@ -22,11 +34,12 @@ export const BalanceStyled = styled.div`
 
 export const Text = styled.div`
   text-transform: uppercase;
-  margin-top: 8px;
+  padding-top: 8px;
   margin-bottom: 12px;
   margin-left: 32px;
   font-size: 12px;
-  color: var(--white);
+  color: var(--transp-40);
+  
 
   @media screen and (min-width: 768px) and (max-width: 1279px) {
     margin-left: 40px;
@@ -46,9 +59,11 @@ export const Symbol = styled.div`
   font-size: 30px;
   color: var(--white);
   margin-left: 32px;
+  
 
   @media screen and (min-width: 768px) and (max-width: 1279px) {
     margin-left: 40px;
+    padding-bottom: 12px;
   }
 
   @media screen and (min-width: 1280px) {
@@ -61,4 +76,11 @@ export const Total = styled.div`
   color: var(--white);
   margin-left: 10px;
   font-weight: 700;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  max-width: 240px;
+
+  @media screen and (min-width: 768px) and (max-width: 1279px) {
+    padding-bottom: 12px;
+  }
 `;
