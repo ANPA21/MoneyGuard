311912bf85701245019bca5edc60a53ac946f7d1
Omelianov Nicolai <omelianov.n@gmail.com>
2023-09-13 04:09:33 +0300

    added div to position navigate and balance

--- src/components/dashboard/dynamic_sidebar/DynamicSidebar.js
+++ src/components/dashboard/dynamic_sidebar/DynamicSidebar.js
@@ -1,13 +1,17 @@
 import { DynamicSidebarStyled } from './DynamicSidebar.styled';
+import { DivForNavigBalancyStaled } from './DivForNavigBalancyStaled.styled';
 import { Navigation } from './navigation/Navigation';
 import { Balance } from './balance/Balance';
 import { Currency } from './currency/Currency';
 
+
 export const DynamicSidebar = () => {
   return (
     <DynamicSidebarStyled>
-      <Navigation />
-      <Balance />
+      <DivForNavigBalancyStaled>
+        <Navigation />
+        <Balance />
+      </DivForNavigBalancyStaled>
       <Currency />
     </DynamicSidebarStyled>
   );
