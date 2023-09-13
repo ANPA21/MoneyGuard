8b42e990ab2715edf8dcd74b61a7664fe4b54691
Omelianov Nicolai <omelianov.n@gmail.com>
2023-09-13 04:13:00 +0300

    added laptop media queries fixed bg chart and position

--- src/components/dashboard/dynamic_sidebar/currency/Currency.styled.js
+++ src/components/dashboard/dynamic_sidebar/currency/Currency.styled.js
@@ -5,6 +5,15 @@ export const Table = styled.div`
   width: 100%;
   border-collapse: collapse;
   margin-bottom: 10px;
+  background-color: rgba(74, 86, 226, 0.10);
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+
+
+  @media screen and (min-width: 768px) and (max-width: 1279px) {
+      margin-bottom: 0;
+
+    }
 `;
 
 export const Row = styled.div`
@@ -18,20 +27,33 @@ export const Row = styled.div`
   &:first-child {
     background-color: rgba(255, 255, 255, 0.2);
     font-weight: 600;
+
+    @media screen and (min-width: 768px) and (max-width: 1279px) {
+      padding: 20px 2px 20px 0;
+    }
   }
 
   &:not(:first-child) {
     font-weight: 400;
-    background: rgba(74, 86, 226, 0.1);
 
     @media screen and (min-width: 768px) and (max-width: 1279px) {
+      display: flex;
+      justify-content: space-between;
       font-family: Circe;
+      height: 0;
+      padding: 20px 0 7px 0;
+
     }
   }
 
   @media screen and (min-width: 768px) and (max-width: 1279px) {
     width: 336px;
-    height: 48px;
+    height: 46px;
+    padding: 0;
+
+    &:last-child {
+    padding-bottom: 0;
+  }
   }
 
   @media screen and (min-width: 1280px) {
@@ -45,6 +67,12 @@ export const Cell = styled.div`
   padding: 8px;
   text-align: center;
   color: var(--white);
+
+@media screen and (min-width: 768px) and (max-width: 1279px) {
+  padding: 12px 20px;
+   
+  }
+
 `;
 
 export const Graph = styled.div``;
