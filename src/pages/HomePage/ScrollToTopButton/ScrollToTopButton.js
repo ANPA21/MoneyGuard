285bbd21c01b04049394d9083659da4c5668d5d6
Hordiyenko Tetyana <hordit@me.com>
2023-09-14 16:08:58 +0200

    add ScrollToTopButton in HomePage fof mobile

---
+++ src/pages/HomePage/ScrollToTopButton/ScrollToTopButton.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { ScrollButton } from './ScrollToTopButton.styled';
+
+export const ScrollToTopButton = () => {
+    const [isVisible, setIsVisible] = useState(false);
+  
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+  
+    const scrollToTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    };
+  
+    useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+  
+    return (
+      <ScrollButton isVisible={isVisible} onClick={scrollToTop}>
+        ↑
+      </ScrollButton>
+    );
+  };
+  
+
